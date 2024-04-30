840fa09324887f62da13d8142ce55f711bc59def
Leticia Baldin <leticiagalvani2003@gmail.com>
2024-04-30 12:51:49 -0300

    feat: add search component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 const Header = () => {
   return (
     <div className="flex justify-between pt-6 px-5">
-      <Image src="/assets/images/logo.png" alt="Fsw food" height={30} width={100} />
+      <Image src="/assets/logo.png" alt="Fsw food" height={30} width={100} />
       <Button size="icon" variant="outline" className="bg-transparent border-none"></Button>
       <MenuIcon/>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
+   <>
    <Header/>
+   <div className="px-5 pt-6">
+   <Search />
+   </div>
+  
+   </>
   );
 }
